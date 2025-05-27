b638d5b521b2dd33a5c6bae866fb502b7385c3b1
DSYS <dinodimanjith404@gmail.com>
2025-05-27 10:04:07 +0530

    context file created and add Route for each pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,35 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/student/Home";
+import CourseList from "./pages/student/CourseList";
+import CourseDetails from "./pages/student/CourseDetails";
+import MyEnrollment from "./pages/student/MyEnrollment";
+import Player from "./pages/student/Player";
+import Loading from "./components/student/Loading";
+import Educator from "./pages/educator/Educator";
+import Dashboard from "./pages/educator/Dashboard";
+import AddCourse from "./pages/educator/AddCourse";
+import MyCourse from "./pages/educator/MyCourse";
+import StudentEnrolled from "./pages/educator/StudentEnrolled";
 
 const App = () => {
   return (
     <div>
-      <h1>App</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/course-list" element={<CourseList />} />
+        <Route path="/course-list/:id" element={<CourseList />} />
+        <Route path="/course/:id" element={<CourseDetails />} />
+        <Route path="/my-enrollments" element={<MyEnrollment />} />
+        <Route path="/player/:courseId" element={<Player />} />
+        <Route path="/loading/:path" element={<Loading />} />
+        <Route path="/educator" element={<Educator />}>
+          <Route path="educator" element={<Dashboard />} />
+          <Route path="add-course" element={<AddCourse />} />
+          <Route path="my-courses" element={<MyCourse />} />
+          <Route path="student-enrolled" element={<StudentEnrolled />} />
+        </Route>
+      </Routes>
     </div>
   );
 };
--- client/src/pages/educator/Educator.jsx
+++ client/src/pages/educator/Educator.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
 
 const Educator = () => {
   return (
     <div>
       <h1>Educator</h1>
+      {<Outlet />}
     </div>
   );
 };
