a227062dd1cdef0152795d93f092a08f561f9990
DSYS <dinodimanjith404@gmail.com>
2025-05-27 16:16:31 +0530

    completed half work of course section in home page

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -2,7 +2,10 @@ import { createContext } from "react";
 export const AppContext = createContext();
 
 export const AppContextProvider = (props) => {
-  const value = {};
+  const currency = import.meta.env.VITE_CURRENCY;
+  const value = {
+    currency,
+  };
 
   return (
     <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
