996085af5891df9176d0f8d0784c13531885e110
DSYS <dinodimanjith404@gmail.com>
2025-06-03 08:07:54 +0530

    sidebar and navbar completed in educator dashboard

--- client/src/pages/educator/Dashboard.jsx
+++ client/src/pages/educator/Dashboard.jsx
@@ -1,6 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { AppContext } from "../../context/AppContext";
+import { dummyDashboardData } from "../../assets/assets";
 
 const Dashboard = () => {
+  const { currency } = useContext(AppContext);
+  const { dashboardData, setDashboardData } = useState(null);
+
+  const fetchDashboardData = (async = () => {
+    setDashboardData(dummyDashboardData);
+  });
+
+  useEffect(() => {
+    fetchDashboardData();
+  }, []);
   return (
     <div>
       <h1>Dashboard</h1>
