1fdc7987c0e079deb7dc9efb510df8ad7bdc7d05
DSYS <dinodimanjith404@gmail.com>
2025-06-05 09:59:02 +0530

    Bug fixed in updateUserCourseProgress

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -5,6 +5,7 @@ import {
   getUserData,
   purchaseCourse,
   userEnrolledCourses,
+  updateUserCourseProgress,
 } from "../controllers/userController.js";
 
 const userRouter = express.Router();
@@ -13,7 +14,7 @@ userRouter.get("/data", getUserData);
 userRouter.get("/enrolled-courses", userEnrolledCourses);
 userRouter.post("/purchase", purchaseCourse);
 
-userRouter.post("/update-course-progress", updateCourseProgress);
+userRouter.post("/update-course-progress", updateUserCourseProgress);
 userRouter.post("/get-course-progress", getUserCourseProgress);
 userRouter.post("/add-rating", addUserRating);
 export default userRouter;
