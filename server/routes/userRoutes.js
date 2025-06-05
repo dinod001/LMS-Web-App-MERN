ff976f5605975c4f0fe199306ac669d47f30fb1a
DSYS <dinodimanjith404@gmail.com>
2025-06-05 08:26:48 +0530

    backenfd completed

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,5 +1,7 @@
 import express from "express";
 import {
+  addUserRating,
+  getUserCourseProgress,
   getUserData,
   purchaseCourse,
   userEnrolledCourses,
@@ -11,4 +13,7 @@ userRouter.get("/data", getUserData);
 userRouter.get("/enrolled-courses", userEnrolledCourses);
 userRouter.post("/purchase", purchaseCourse);
 
+userRouter.post("/update-course-progress", updateCourseProgress);
+userRouter.post("/get-course-progress", getUserCourseProgress);
+userRouter.post("/add-rating", addUserRating);
 export default userRouter;
